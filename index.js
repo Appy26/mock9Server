f6aae7a1ef7e1c2d8d622a6cf982a5ea87b29e9c
Appy26 <aparnaharsh2604@gmail.com>
2023-06-07 16:04:40 +0530

    fixing err

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.send({"msg":"hello world"})
 })
 app.use(authentication)
 app.use("/user", userRouter)
